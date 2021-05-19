d31d22a324edeb037b4378eb3c9b861bd7bff82f
Lauren Clements <laurenclements65@gmail.com>
2021-05-19 15:59:43 -0500

    update html&css on all pages, basic js login

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//  ---------  nav setup  --------- //
+//  -----------------------  nav setup  -----------------------   //
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
@@ -22,3 +22,22 @@ const navSlide = () => {
 };
 
 navSlide();
+
+//  -----------------------  login setup  -----------------------   //
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
